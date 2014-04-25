ecea350c01b8bd8f11f32b471c3482526a96e836
rdrew <robdrewupei@gmail.com>
2014-04-25 15:59:13 -0300

    1st commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is new Drupal theme for Robertson Library, UPEI. It is based on AT Subtheme (Corolla > Footheme).
+
+Rob Drew
---
+++ sites/all/themes/roblib_theme/template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "roblib_theme" to match
+ * your subthemes name, e.g. if you name your theme "roblib_theme" then the function
+ * name will be "roblib_theme_preprocess_hook". Tip - you can search/replace
+ * on "roblib_theme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function roblib_theme_preprocess_html(&$vars) {
+}
+function roblib_theme_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function roblib_theme_preprocess_page(&$vars) {
+}
+function roblib_theme_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function roblib_theme_preprocess_node(&$vars) {
+}
+function roblib_theme_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function roblib_theme_preprocess_comment(&$vars) {
+}
+function roblib_theme_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function roblib_theme_preprocess_block(&$vars) {
+}
+function roblib_theme_process_block(&$vars) {
+}
+// */
